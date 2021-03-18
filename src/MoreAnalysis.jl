bf8c1c41790a1b2cab6ae7de274fa833cd3de647
Fons van der Plas <fonsvdplas@gmail.com>
2021-03-18 23:15:23 +0100

    match refactor in Pluto source code

--- src/MoreAnalysis.jl
+++ src/MoreAnalysis.jl
@@ -61,7 +61,7 @@ end
 
 function upstream_recursive!(found::Set{Cell}, notebook::Notebook, topology::NotebookTopology, from::Vector{Cell})
     for cell in from
-        references = topology[cell].references
+        references = topology.nodes[cell].references
         for upstream in Pluto.where_assigned(notebook, topology, references)
             if upstream ∉ found
                 push!(found, upstream)
@@ -74,7 +74,7 @@ end
 "All cells that can affect the outcome of changing the given variable."
 function codependents(notebook::Notebook, topology::NotebookTopology, var::Symbol)
     assigned_in = filter(notebook.cells) do cell
-        var ∈ topology[cell].definitions
+        var ∈ topology.nodes[cell].definitions
     end
     
     downstream = collect(downstream_recursive(notebook, topology, assigned_in))
@@ -88,12 +88,12 @@ For each key (a bound symbol), the value is the list of (other) bound variables 
 function bound_variable_connections_graph(notebook::Notebook)::Dict{Symbol,Vector{Symbol}}
     topology = notebook.topology
     bound_variables = union(map(notebook.cells) do cell
-        find_bound_variables(cell.parsedcode)
+        find_bound_variables(topology.codes[cell].parsedcode)
     end...)
     Dict{Symbol,Vector{Symbol}}(
         var => let
             cells = codependents(notebook, topology, var)
-            defined_there = union!(Set{Symbol}(), (topology[c].definitions for c in cells)...)
+            defined_there = union!(Set{Symbol}(), (topology.nodes[c].definitions for c in cells)...)
             # Set([var]) ∪ 
             collect((defined_there ∩ bound_variables))
         end
