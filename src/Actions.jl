570a117e2f020bf1b45ecdb7830170bca14b3a6c
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-04-24 22:27:26 +0300

    Webhook

--- src/Actions.jl
+++ src/Actions.jl
@@ -3,14 +3,24 @@ module Actions
     using Base64
     using SHA
     using FromFile
+    export myhash, path_hash, showall, add_to_session!, renew_session!, remove_from_session!, register_webhook, run_folder, get_paths_from, generate_static_export
     @from "./MoreAnalysis.jl" import MoreAnalysis 
     @from "./Export.jl" using Export
     @from "./Types.jl" using Types
     @from "./FileHelpers.jl" import FileHelpers: find_notebook_files_recursive
     myhash = base64encode ∘ sha256
+    path_hash = path -> myhash(read(path))
+
     showall(xs) = Text(join(string.(xs),"\n"))
 
-    function add_to_session!(notebook_sessions, server_session, path, settings, run_server=true, start_dir=".")
+    """
+    Core Action. 
+    
+    add_to_session! lets PlutoSliderServer know about a new session
+    start_dir should come from settings
+    """
+    function add_to_session!(notebook_sessions, server_session, path, settings, run_server, start_dir)
+        # TODO: Take these from Settings
         jl_contents = read(joinpath(start_dir, path), String)
         hash = myhash(jl_contents)
         # The 5 lines below are needed if the code is not invoked within PlutoSliderServer.run_directory
@@ -25,6 +35,7 @@ module Actions
         local notebook, original_state
 
         cached_state = skip_cache ? nothing : try_fromcache(settings.Export.cache_dir, hash)
+        # This probably only works in github action context
         if cached_state !== nothing
             @info "Loaded from cache, skipping notebook run" hash
             original_state = cached_state
@@ -68,8 +79,46 @@ module Actions
         notebook_sessions[i], jl_contents, original_state
     end
 
-    function renew_session!(notebook_sessions, server_session, path)
+    """
+    Wait for notebook to have 0 running or queued cells
+    Poll in intervals of 5 seconds
+    """
+    function waitnotebookready(notebook::Pluto.Notebook)
+        i = 0
+        sleep(3)  # "Make sure" pluto picked up the change in notebook 
+        println("Waiting for notebook to get ready")
+        while (i < 360)
+            isrunning = length(filter(cell -> (cell.queued || cell.running), notebook.cells)) > 0
+            if (!isrunning)
+                println("")
+                return
+            end
+            print("\r\nWaiting for notebook to be ready [$(5*i)s]")
+            sleep(5)
+            i += 1
+        end
+        @error "Couldn't get the notebook status after 30 minutes."
+    end
+
+    """
+    Core Action. Renew a session without restarting it!
+
+    This function renews the RunningNotebookSession that the PlutoSliderServer
+    tracks with
+    1. updated hash (serve correct GET requests)
+    2. updated bond_connections
+    3. update original_state (will be used in export, if that is set)
+    This implementation assumes Pluto will watch file updates
+    There is a race condition there:
+        Webhook
+            -> pull
+            -> file changes
+                -> pluto picksup the change [is the file ready?]
+            -> renew_session [is pluto running? has pluto picked up file change?]
+    """
+    function renew_session!(notebook_sessions, server_session, path, settings)
         @info "Renewing " path
+        jl_contents = read(joinpath(settings.SliderServer.start_dir, path), String)
         new_hash = path_hash(path)
         i = findfirst(s -> s.path == path, notebook_sessions)
         if isnothing(i)
@@ -77,7 +126,7 @@ module Actions
             return (nothing, nothing, nothing)
         end
         session = notebook_sessions[i]
-        ## DID NOTEBOOK GET UPDATED?
+        waitnotebookready(session.notebook)
         bond_connections = MoreAnalysis.bound_variable_connections_graph(session.notebook)
         original_state = Pluto.notebook_to_js(session.notebook)
         notebook_sessions[i] = RunningNotebookSession(;
@@ -87,8 +136,11 @@ module Actions
              original_state,
              bond_connections,
          )
+         notebook_sessions[i], jl_contents, original_state
     end
-
+    """
+    Core Action. Stops a notebook from running in PlutoSliderServer
+    """
     function remove_from_session!(notebook_sessions, server_session, hash)
         i = findfirst(notebook_sessions) do sesh
             sesh.hash === hash
@@ -106,8 +158,12 @@ module Actions
         )
     end
 
+    """
+    Starts and creates static exports for all notebooks in a folder,
+    respecting the settings provided
+    """
     function run_folder(folder, notebook_sessions, server_session, settings, output_dir)
-        to_run = get_paths_from(folder, settings)[1:3]
+        to_run = get_paths_from(folder, settings)
         for path in to_run
             @info path "starting"
             session, jl_contents, original_state = add_to_session!(notebook_sessions, server_session, path, settings, true, folder)
@@ -118,11 +174,24 @@ module Actions
         @info "success"
     end
 
+    """
+    Helper function to get the paths in a folder , excluding those
+    set for exclusion in settings
+    """
     function get_paths_from(folder, settings)
         notebook_paths = find_notebook_files_recursive(folder)
         to_run = setdiff(notebook_paths, settings.SliderServer.exclude)
     end
 
+    """
+    Core Action: Generate static export for a Pluto Notebook
+    Settings must specify
+    1. slider_server_url: URL of the slider server. This will be the URL of your server, if you deploy
+    2. offer_binder: Flag to enable the Binder button
+    3. binder_url: URL of the binder link that will be invoked. Use a compatible pluto-enabled binder 
+    4. baked_state: Whether to export pluto state within the html or in a separate file.
+    5. pluto_cdn_root: URL where pluto will go to find the static frontend assets 
+    """
     function generate_static_export(path, settings, original_state, output_dir, jl_contents)
         pluto_version = Export.try_get_exact_pluto_version()
         export_jl_path = let
