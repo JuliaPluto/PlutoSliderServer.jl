5e0ab9282453aecb9401d0d65226507d1bdb1a66
Fons van der Plas <fonsvdplas@gmail.com>
2022-09-03 12:59:25 +0200

    dsff

--- test/huh.jl
+++ test/huh.jl
@@ -1,23 +1,34 @@
 import HTTP
 import Sockets
 
-port = rand(12345:65000)
-host = "127.0.0.1"
+
+function hello()
+    port = rand(12345:65000)
+    host = "127.0.0.1"
 
 
 
-hostIP = parse(Sockets.IPAddr, host)
-serversocket = Sockets.listen(hostIP, UInt16(port))
+    hostIP = parse(Sockets.IPAddr, host)
+    serversocket = Sockets.listen(hostIP, UInt16(port))
 
 
-@info "# Starting server..."
+    @info "# Starting server..."
 
 
-http_server = HTTP.serve!(hostIP, UInt16(port), server=serversocket) do req
-    return HTTP.Response(200)
+    http_server = HTTP.serve!(hostIP, UInt16(port), server=serversocket) do req
+        return HTTP.Response(200)
+    end
+
+    @info "# Server started"
+
+
+    close(http_server)
+
 end
 
-@info "# Server started"
 
-
-close(http_server)
+@async try
+    hello()
+catch e
+    @error "Error in server" exception = (e, catch_backtrace())
+end
