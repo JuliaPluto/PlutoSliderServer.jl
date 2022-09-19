3ec3c5bf0f252f76d1e250830e4213d2bd26fe56
Fons van der Plas <fonsvdplas@gmail.com>
2022-09-19 15:00:44 +0200

    Use frontmatter in HTML export, fix #89 (#90)

--- test/dir1/a.jl
+++ test/dir1/a.jl
@@ -1,6 +1,15 @@
 ### A Pluto.jl notebook ###
 # v0.12.123123
 
+#> [frontmatter]
+#> title = "My<Title"
+#> author_url = "https://github.com/JuliaPluto"
+#> image = "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
+#> tags = ["aaa", "bbb"]
+#> author_name = "Pluto.jl"
+#> description = "ccc"
+#> license = "Unlicense"
+
 using Markdown
 using InteractiveUtils
 
