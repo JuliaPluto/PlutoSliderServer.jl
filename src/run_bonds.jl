0ac075938e4ddc6fbc3c2cc17149a9a883aa0f7c
Fons van der Plas <fonsvdplas@gmail.com>
2025-11-11 15:37:04 +0100

    merge

--- src/run_bonds.jl
+++ src/run_bonds.jl
@@ -10,16 +10,69 @@ function run_bonds_get_patches(
     server_session::Pluto.ServerSession,
     run::RunningNotebook,
     bonds::AbstractDict{Symbol,<:Any},
-)::Union{AbstractDict{String,Any},Nothing}
-
+    explicits::Union{Nothing,Set{Symbol}},
+)
     notebook = run.notebook
 
-    topological_order, new_state = withtoken(run.token) do
+    names::Vector{Symbol} = Symbol.(keys(bonds))
+    # "explicits" defaults to "all bonds"
+    explicits = @something(explicits, Set(names))
+    
+    names_original = names
+    names =
+        begin
+            # REMOVE ALL NAMES that depend on 
+            # an explicit bond
+            # 
+            # Because its new value might no longer be valid.
+            # 
+            # For exaxmple:
+            # @bind xx Slider(1:100)
+            # @bind yy Slider(xx:100)
+            # 
+            # (ignore bond transformatinos for now)
+            # 
+            # The sliders will be set on (1,1) initially.
+            # The user moves the first slider, giving (10,1).
+            # The value for `y` will be sent, but this should be ignored. Because it was generated from an outdated bond.
+            
+            first_layer = PlutoDependencyExplorer.where_referenced(
+                notebook.topology,
+                explicits,
+            )
+            
+            next_layers = Pluto.MoreAnalysis.downstream_recursive(
+                notebook.topology,
+                first_layer,
+            )
+
+            # all cells that depend on an explicit bond
+            cells_depending_on_explicits = union!(first_layer, next_layers)
+
+            # remove any variable `n` from `names` if...
+            filter(names) do n
+                !(
+                    # ...`n` depends on an explicit bond.
+                    any(cells_depending_on_explicits) do c
+                        n in notebook.topology.nodes[c].definitions
+                    end
+                )
+            end
+        end
+        
+    t35 = time()
+        
+        
+    id(c) = c.cell_id
+
+    @debug "Analysis" names names_original id.(cells_depending_on_explicits)
+    
+    new_state = withtoken(sesh.run.token) do
         try
+            # Set the bond values. We don't need to merge dicts here because the old bond values will never be used.
             notebook.bonds = bonds
-
-            names::Vector{Symbol} = Symbol.(keys(bonds))
-
+            
+            # Run the bonds!
             topological_order = Pluto.set_bond_values_reactive(
                 session=server_session,
                 notebook=notebook,
@@ -28,39 +81,56 @@ function run_bonds_get_patches(
                 run_async=false,
             )::Pluto.TopologicalOrder
 
-            new_state = Pluto.notebook_to_js(notebook)
-
-            topological_order, new_state
+            @debug "Finished running!" length(topological_order.runnable)
+            
+            Pluto.notebook_to_js(notebook)
         catch e
             @error "Failed to set bond values" exception = (e, catch_backtrace())
-            nothing, nothing
+            nothing
         end
     end
-    if topological_order === nothing
-        return nothing
-    end
-
-    ids_of_cells_that_ran = [c.cell_id for c in topological_order.runnable]
-
-    @debug "Finished running!" length(ids_of_cells_that_ran)
+    new_state === nothing && return (
+        HTTP.Response(500, "Failed to set bond values") |>
+        with_cors! |>
+        with_not_cacheable!
+    )
+    
+    t4 = time()
 
     # We only want to send state updates about...
     function only_relevant(state)
         new = copy(state)
         # ... the cells that just ran and ...
         new["cell_results"] = filter(state["cell_results"]) do (id, cell_state)
-            id ∈ ids_of_cells_that_ran
+            id ∈ (c.cell_id for c in cells_depending_on_explicits)
         end
-        # ... nothing about bond values, because we don't want to synchronize among clients.
-        new["bonds"] = Dict{String,Dict{String,Any}}()
+        # ... nothing about bond values, because we don't want to synchronize among clients. and...
+        delete!(new, "bonds")
+        # ... we ignore changes to the status tree caused by a running bonds.
+        delete!(new, "status_tree")
         new
     end
 
-    patches = Firebasey.diff(only_relevant(run.original_state), only_relevant(new_state))
-    patches_as_dicts::Array{Dict} = Firebasey._convert(Array{Dict}, patches)
+    patches = let
+        notebook_patches = Firebasey.diff(
+            only_relevant(sesh.run.original_state),
+            only_relevant(new_state),
+        )
 
-    Dict{String,Any}(
-        "patches" => patches_as_dicts,
-        "ids_of_cells_that_ran" => ids_of_cells_that_ran,
+        # Remove bonds that depend on explicit bonds. Because this means that the bond was re-created during this run, and its value must be reset,
+        bond_patches = [
+            Firebasey.RemovePatch(["bonds", string(k)]) for
+            k in keys(bonds) if k ∉ names
+        ]
+
+        @debug "patches" notebook_patches bond_patches
+
+        union!(notebook_patches, bond_patches)
+    end
+    
+    return (;
+        patches,
+        t35,
+        t4,
     )
 end
